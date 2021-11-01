3127ca4e41402dd44151a6debf1767243f3333c3
Zmiter <ozmiter@gmail.com>
2021-11-01 11:27:53 +0300

    feat: add and connect userList component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import {Provider} from "react-redux";
 import {store} from "./store";
+import UserList from "./components/userList";
 
 function App() {
   return (
       <Provider store={store}>
         <div className="App">
-
+            <UserList />
         </div>
       </Provider>
 
