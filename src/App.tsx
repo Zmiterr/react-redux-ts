0b22f65811a42f38c82bb3734458e126f32619b2
Zmiter <ozmiter@gmail.com>
2021-11-01 10:03:29 +0300

    feat: create store and user reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 function App() {
   return (
-    <div className="App">
+      <Provider store={store}>
+        <div className="App">
 
-    </div>
+        </div>
+      </Provider>
+
   );
 }
 
---
+++ src/store/reducers/userReducer.ts
@@ -0,0 +1,52 @@
+interface UserState {
+    users: any[],
+    isLoading: boolean,
+    error: null | string
+}
+
+// interface UserAction {
+//     type: string,
+//     payload?: any
+// }
+
+interface FetchUserInterface {
+    type: UserActionsTypes.FETCH_USERS
+    // payload?: any
+}
+
+interface FetchUserSuccessInterface {
+    type: UserActionsTypes.FETCH_USERS_SUCCESS
+    payload: any[]
+}
+
+interface FetchUserErrorInterface {
+    type: UserActionsTypes.FETCH_USERS_ERROR
+    payload: string
+}
+
+export enum UserActionsTypes {
+     FETCH_USERS = 'FETCH_USERS',
+     FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS',
+     FETCH_USERS_ERROR = 'FETCH_USERS_ERROR'
+}
+
+
+const initialState: UserState = {
+    users: [],
+    isLoading: false,
+    error: null
+}
+
+type UserAction =   FetchUserInterface | FetchUserSuccessInterface | FetchUserErrorInterface
+
+export const userReducer = (state = initialState, action: UserAction): UserState => {
+    switch (action.type) {
+        case UserActionsTypes.FETCH_USERS:
+            return {users: [], isLoading: true,error: null}
+        case UserActionsTypes.FETCH_USERS_SUCCESS:
+            return {users: action.payload, isLoading: false,error: null}
+        case UserActionsTypes.FETCH_USERS_ERROR:
+            return {users: [], isLoading: false,error: action.payload}
+        default: return state
+    }
+}
